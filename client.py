706a893246271984b256ca235438bedc46798ede
Aryaman Prakash <123716170+Rya-man@users.noreply.github.com>
2025-09-18 01:26:19 +0530

    added comment for server.py

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import socket
-
+""" So now we will be assigning the host ip after a device connects to it, as of now it is static.
+    later on we can make it dynamic, in this case the host ip is the ip address of the device suppling the hotspot.
+    the connected devices can talk to the host device only
+    """
 HOST = "10.130.40.223"  # your Android hotspot IP (gateway)
 PORT = 5000
 
